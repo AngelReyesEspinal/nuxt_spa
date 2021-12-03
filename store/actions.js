3a1566e0a4a6320e745c226614475b6757d86a8e
Angel Arsenio Reyes Espinal <angel.reyes@solvex.com.do>
2021-12-03 16:19:04 -0400

    commit

--- store/actions.js
+++ store/actions.js
@@ -8,14 +8,13 @@ import {
 export const fetch_sgt_data = async ({ state, commit, dispatch }) => {
     await dispatch('fetch_preseason_games');
     await dispatch('fetch_season_games');
-
-    /*
+    await dispatch('fetch_schedule');
+    await dispatch('fetch_giveaways');
     commit('append_tooltip_to_games');
     commit('filter_sgt_data_by_date');
     commit('set_games', state.all);
     commit('set_months');
     commit('set_teams_by_filtered_data');
-    */
 }
 
 export const fetch_preseason_games = async ({ state, commit, dispatch }) => {
@@ -42,7 +41,6 @@ export const fetch_preseason_games = async ({ state, commit, dispatch }) => {
 }
 
 export const fetch_season_games = async ({ state, commit, dispatch }) => {
-    
     if (state.season_data.length === 0) {
         let json = {};
         try {
@@ -52,11 +50,43 @@ export const fetch_season_games = async ({ state, commit, dispatch }) => {
             console.log(ex);
         } finally {
             const games = json.gscd.g.filter((game) => game.h.tn == "Clippers");
-            state.all.push(...games);
-            state.all.forEach((game) => {
-                game.ticketInfo = { axs: null };
-            });
+            commit('concat_season_games', games);
             commit('set_season_data', games);
+        }
+    }
+}
+
+export const fetch_schedule = async ({ state, commit, dispatch }) => {
+    if (state.schedule_data.length === 0) {
+        let json = {};
+        try {
+          const response = await fetch(schedule);
+          json = await response.json();
+        } catch (ex) {
+          console.log(ex);
+        } finally {
+          const filteredSchedule = [];
+          state.all.forEach((game) => {
+            const item = json.find((g) => g.gameId === game.gid);
+            if (item) 
+              filteredSchedule.push(item);
+          });
+          commit('attach_schedule', json);
+          commit('set_schedule_data', filteredSchedule);
+        }
+    }
+}
+
+export const fetch_giveaways = async ({ state, commit, dispatch }) => {
+    if (state.giveaways.length === 0) {
+        let json = {};
+        try {
+          const response = await fetch(giveaways);
+          json = await response.json();
+        } catch (ex) {
+          console.log(ex);
+        } finally {
+          commit('set_giveaways', json);
         }
     }
 }
